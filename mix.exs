a9381ad956577b82cd14399456c6d90450389520
Adriel Radicchi <adriel.nfsc@gmail.com>
2022-02-08 13:10:25 -0300

    updated all dependecy

--- mix.exs
+++ mix.exs
@@ -5,38 +5,38 @@ defmodule Blaze.MixProject do
     [
       app: :blaze,
       description: "A friendlier interface to Google Cloud Firestore.",
-      version: "0.1.0",
-      elixir: "~> 1.10",
+      version: "0.2.0",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
-      source_url: "https://github.com/kolorahl/blaze",
+      source_url: "https://github.com/kolorahl/blaze"
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:google_api_firestore, "~> 0.21"},
-      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
+      {:google_api_firestore, "~> 0.23"},
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false}
     ]
   end
 
   defp package do
     [
       licenses: [
-        "GNU GPLv3",
+        "GNU GPLv3"
       ],
       links: %{
         "GitHub" => "https://github.com/kolorahl/blaze"
-      },
+      }
     ]
   end
 end
